c9849543324b9749d9b3a6b33a45f4b75165efb6
Alyssa Williams <awilliams62090@gmail.com>
2019-10-04 23:10:57 -0600

    Updates

---
+++ precache-manifest.0330222e9d6a2bf7c02d6740578db48a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab2cff0a626c30418c93",
+    "url": "/reactCrystalGame/static/js/runtime~main.ab2cff0a.js"
+  },
+  {
+    "revision": "c901c6b84ea8ba4b3783",
+    "url": "/reactCrystalGame/static/js/main.c901c6b8.chunk.js"
+  },
+  {
+    "revision": "9fcf65a891d3c07f5a21",
+    "url": "/reactCrystalGame/static/js/2.9fcf65a8.chunk.js"
+  },
+  {
+    "revision": "c901c6b84ea8ba4b3783",
+    "url": "/reactCrystalGame/static/css/main.e4ae0842.chunk.css"
+  },
+  {
+    "revision": "980714f3785d7e9b07acdba933a954f6",
+    "url": "/reactCrystalGame/index.html"
+  }
+];
